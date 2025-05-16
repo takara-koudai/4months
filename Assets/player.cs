91cc540b509bf50823b59554d98b5da444ab1840
kodai <kodai@MSI>
2025-05-16 12:17:23 +0900

    プレイヤー動き制作

--- Assets/player.cs
+++ Assets/player.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    
+    public Rigidbody2D rb;
+
+    float moveSpeed = 4f;
+    float jampspeed = 8f;
+
+    private bool isJamp;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //ç∂Ç…ìÆÇ≠
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+
+        //âEÇ…ìÆÇ≠
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        //ÉWÉÉÉìÉv
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, jampspeed);
+            isJamp = true;
+        }
+
+
     }
 }
