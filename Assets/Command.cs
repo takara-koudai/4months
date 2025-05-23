c62697ef8bdf2a94ff3b8f4524ffde47eb50e974
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2025-05-23 15:08:05 +0900

    player

--- Assets/Command.cs
+++ Assets/Command.cs
@@ -1,22 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Command : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject command;
-    bool flag = false;
+    public Text text;
+    string[] texts = {"右にいけ","左に行け" };
+    private string currentInstruction;
 
     void Start()
     {
-        flag = false;
+        GenerateInstruction();
     }
 
+    void GenerateInstruction()
+    {
+
+        currentInstruction = texts[Random.Range(0, texts.Length)];
+        text.text = currentInstruction;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    public void OnPlayerMove(string direction)
+    {
+
+        bool isCorrect = (currentInstruction.Contains("Not") && !currentInstruction.Contains(direction) ||
+            (!currentInstruction.Contains("NOT") && currentInstruction.Contains(direction)));
+
+        if (isCorrect)
+        {
+            Debug.Log("正解");
+            GenerateInstruction();
+        }
+
+        else
+        {
+            Debug.Log("ゲームオーバー");
+        }
 
     }
+
 }
